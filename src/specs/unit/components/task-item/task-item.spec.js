a4ae315bb0b49832d5ed29cbce009a2a7ff3d6de
Leonardo Pinheiro <leonardo.pinheiro@ruptiva.com>
2019-05-23 21:22:35 -0300

    fix (specs) folders and files rename to task

--- src/specs/unit/components/task-item/task-item.spec.js
+++ src/specs/unit/components/task-item/task-item.spec.js
@@ -2,26 +2,26 @@ import React from 'react'
 import { render, fireEvent } from 'react-testing-library'
 import 'jest-styled-components'
 
-import TodoItem from 'components/todo-item'
+import TaskItem from 'components/task-item'
 
-describe('[COMPONENTS] TodoItem', () => {
+describe('[COMPONENTS] TaskItem', () => {
   it('Should render correctly', () => {
-    const { container } = render(<TodoItem time='15 minutos' label='Fazer café' done toggle={jest.fn()} />)
+    const { container } = render(<TaskItem time='15 minutos' label='Fazer café' done toggle={jest.fn()} />)
     expect(container).toMatchSnapshot()
   })
 
   it('Should check if a task is not completed', () => {
-    const { container } = render(<TodoItem time='15 minutos' label='Fazer café' done={false} toggle={jest.fn()} />)
+    const { container } = render(<TaskItem time='15 minutos' label='Fazer café' done={false} toggle={jest.fn()} />)
     expect(container.querySelector('#task-situation')).toHaveStyle('background-color: #DA493A')
   })
 
   it('Should check if a task is completed', () => {
-    const { container } = render(<TodoItem time='15 minutos' label='Fazer café' done toggle={jest.fn()} />)
+    const { container } = render(<TaskItem time='15 minutos' label='Fazer café' done toggle={jest.fn()} />)
     expect(container.querySelector('#task-situation')).toHaveStyle('background-color: #2ECC71')
   })
 
   it('Should complete the task when it was clicked 2 times', () => {
-    const { container } = render(<TodoItem time='15 minutos' label='Fazer café' done={false} toggle={jest.fn()} />)
+    const { container } = render(<TaskItem time='15 minutos' label='Fazer café' done={false} toggle={jest.fn()} />)
     expect(container.querySelector('#task-situation')).toHaveStyle('background-color: #2ECC71')
   })
 })
